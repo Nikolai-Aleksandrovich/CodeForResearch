dccf480a933893def678230b0a4f788164f2e1af
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2020-12-07 23:24:39 +0800

    add the function of DBScan for POI

--- essayCode/test4.py
+++ essayCode/test4.py
@@ -1,3 +1,15 @@
-import os
-print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
-print("PATH:", os.environ.get('PATH'))
+
+import numpy as np
+import pandas as pd
+from sklearn.cluster import DBSCAN
+from sklearn import metrics
+
+from geopy.distance import great_circle
+from shapely.geometry import MultiPoint
+import matplotlib.pyplot as plt, time
+df = pd.read_csv("E:/data/ExprimentField/manhatan/Poi_NYC_Manhatan.csv")
+coords = df[['latitude', 'longitude']].values
+for row in df:
+    print(row)
+rs = coords[(lambda row: df[(df['latitude']==row['latitude']) & (df['longitude']==row['longitude'])].iloc[0], axis=1)]
+# print(rs)
