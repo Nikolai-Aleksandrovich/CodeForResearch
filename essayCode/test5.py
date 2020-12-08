d9be4c0f1647517806bbf2f0074e0a9c2de1a0f5
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2020-12-08 13:56:20 +0800

    update DBSCAN function

--- essayCode/test5.py
+++ essayCode/test5.py
@@ -17,12 +17,11 @@ def get_centermost_point(cluster):
 
 df = pd.read_csv("E:/data/ExprimentField/manhatan/Poi_NYC_Manhatan.csv", encoding='utf-8')
 df.head()
-
 coords = df[['latitude', 'longitude']].values
 kms_per_radian = 6371.0088
-epsilon = 0.01 / kms_per_radian
+epsilon = 0.1/ kms_per_radian
 start_time = time.time()
-db = DBSCAN(eps=epsilon, min_samples=1, algorithm='ball_tree', metric='haversine').fit(np.radians(coords))
+db = DBSCAN(eps=epsilon, min_samples=5, algorithm='ball_tree', metric='haversine').fit(np.radians(coords))
 cluster_labels = db.labels_
 num_clusters = len(set(cluster_labels))
 # turn the clusters in to a pandas series, where each element is a cluster of points
@@ -38,16 +37,20 @@ lats, lons = zip(*centermost_points)
 
 rep_points = pd.DataFrame({'lon':lons, 'lat':lats})
 rep_points.tail()
-print(rep_points)
-# rs = rep_points.apply(lambda row: df[(df['latitude']==row['latitude']) & (df['longitude']==row['longitude'])].iloc[0], axis=1)
-# rs.to_csv('data/summer-travel-gps-dbscan.csv', encoding='utf-8')
-# rs.tail()
-#
-# fig, ax = plt.subplots(figsize=[10, 6])
-# rs_scatter = ax.scatter(rs['lon'], rs['lat'], c='#99cc99', edgecolor='None', alpha=0.7, s=120)
-# df_scatter = ax.scatter(df['lon'], df['lat'], c='k', alpha=0.9, s=3)
-# ax.set_title('Full data set vs DBSCAN reduced set')
-# ax.set_xlabel('Longitude')
-# ax.set_ylabel('Latitude')
-# ax.legend([df_scatter, rs_scatter], ['Full set', 'Reduced set'], loc='upper right')
-# plt.show()
+
+rs = rep_points.apply(lambda row: df[(df['latitude']==row['lat']) & (df['longitude']==row['lon'])].iloc[0], axis=1)
+rs.to_csv('E:/data/ExprimentField/manhatan/Poi_NYC_Manhatan_DBSCAN.csv', encoding='utf-8')
+rs.tail()
+
+
+fig, ax = plt.subplots(figsize=[10, 6])
+
+
+rs_scatter = ax.scatter(rs['longitude'], rs['latitude'], c='#99cc99', edgecolor='None', alpha=0.7, s=120)
+df_scatter = ax.scatter(df['longitude'], df['latitude'], c='k', alpha=0.9, s=3)
+ax.set_title('Full data set vs DBSCAN reduced set')
+ax.set_xlabel('Longitude')
+ax.set_ylabel('Latitude')
+ax.legend([df_scatter, rs_scatter], ['Full set', 'Reduced set'], loc='upper right')
+plt.show()
+
