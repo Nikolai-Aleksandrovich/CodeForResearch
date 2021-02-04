fb5ab0265ed6a516026197c797733b1281c7edbc
GitShark <53007941+Nikolai-Aleksandrovich@users.noreply.github.com>
2021-02-04 19:13:27 +0800

    update

--- essayCode/consieSimilarity.py
+++ essayCode/consieSimilarity.py
@@ -100,101 +100,64 @@ def cosine_similarity(x, y, norm=False):
     return 0.5 * cos + 0.5 if norm else cos  # 归一化到[0, 1]区间内
 
 
-def getCosineSimilarityFromOut(filePath1, filePath2, filePath3, k):
-    # start_time = time.time()
-    #测试数据的图文件
+def getCosineSimilarityFromOut(testEdgelist, trainResult, k):
+    start_time = time.time()
+    # 80%训练样本的Result.txt
     try:
-        f = open(filePath3)
+        f = open(trainResult)
     except IOError:
         print("---**---打开文件失败！请检查是否存在该文件！并重新输入文件名!---**---")
-    print("filePath:", filePath3)
-    TestVectorsCount = 0
-    EdgelistNodesCount = 0
+    print("filePath:", trainResult)
+    TrainResultCount = 0
     EdgeListNodeList = []
     for line in f.readlines():
-        EdgelistNodesCount = EdgelistNodesCount + 1
+        TrainResultCount = TrainResultCount + 1
 
         lines = (line.strip().split(" "))
-        TestVectorsCount = TestVectorsCount + 1
         EdgeListNodeList.append(lines)
 
-    EdgeListNode = np.array(EdgeListNodeList, dtype=float)
+    TrainResult = np.array(EdgeListNodeList, dtype=float)
     # print(EdgeListNode)
 
-    # 20%测试样本
+    # 测试数据的edgelist文件
     try:
-        f = open(filePath1)
+        f = open(testEdgelist)
     except IOError:
         print("---**---打开文件失败！请检查是否存在该文件！并重新输入文件名!---**---")
-    print("filePath:", filePath1)
-    TestVectorsCount = 0
+    print("filePath:", testEdgelist)
     TestitemsCount = 0
     TestVectorList = []
     for line in f.readlines():
         TestitemsCount = TestitemsCount + 1
-
         lines = line.strip().split(" ")
-        TestVectorsCount = TestVectorsCount + 1
         TestVectorList.append(lines)
-    TestVectorsArray = np.array(TestVectorList, dtype=float)
+    TestList = np.array(TestVectorList, dtype=float)
     # print(TestVectorsArray)
 
-    # 80%原样本
-    try:
-        f = open(filePath2)
-    except IOError:
-        print("---**---打开文件失败！请检查是否存在该文件！并重新输入文件名!---**---")
-    print("filePath:", filePath2)
-    TrainVectorsCount = 0
-    TrainitemsCount = 0
-    TrainVectorList = []
-    for line in f.readlines():
-        TrainitemsCount = TrainitemsCount + 1
-
-        lines = line.strip().split(" ")
-        TrainVectorsCount = TrainVectorsCount + 1
-        TrainVectorList.append(lines)
-
-    TrainVectorsArray = np.array(TrainVectorList, dtype=float)
-    # SortedTrainVectorsArray = TrainVectorsArray[TrainVectorsArray[:, 0].argsort()]# 对其按照第1列排序
-    # 打印排好序的数组
-    # print(SortedEdgesArray)
-    # #打印原数组
-    # print(edgesArray)
-
-    currentNodeID = ""
-    currentCosineSimilarityList = []
-    testNodeID = []  # 小数据集节点ID
     trainNodeID = []  # 较大训练数据集节点ID
-    testCosineSimilarityList = []
-    allCosineSimilarityList = []
-    finalCSList = [[] for i in range(TestitemsCount)]
-    # finalCSList =[TestitemsCount] [TrainitemsCount]#最终的余弦相似性二维数组
-    a = -1
+
+    finalCSList = [[] for i in range(TrainResultCount)]
+    a = 0
 
     SortedCarList = []
     ValidCount = 0
 
-    for trainArray in TrainVectorsArray:
-        finaltrainVectorFloat = []
-        trainNodeID.append(trainArray[0])  # 当前节点顺序写入较大数据训练集节点节点序列
-
-    for testArray in TestVectorsArray:
-        a = a + 1
+    for trainArray in TrainResult:
         CSListForEachRow = []
-        finalTestVectorFloat = []
-        testNodeID.append(testArray[0])
-        testVector = np.delete(testArray, 0)
-        for trainArray in TrainVectorsArray:
-            trainVector = np.delete(trainArray, 0)
-            CSListForEachRow.append(cos_sim(testVector, trainVector))  # 每行的余弦相似性加入列表
-
+        trainNodeID.append(trainArray[0])
+        trainVector = np.delete(trainArray, 0)
+        for otherArray in TrainResult:
+            if ((otherArray==trainArray).all()):
+                continue
+            otherVector=np.delete(otherArray, 0)
+            CSListForEachRow.append(cos_sim(trainVector, otherVector))  # 每行的余弦相似性加入列表
         finalCSList[a] = CSListForEachRow
+        a=a+1
     # print('start print finalCSList')
     # print(finalCSList)
     # print('end of finalCSList')
-    TopKIndex = [[] for i in range(TestitemsCount)]
-    for i in range(0, TestitemsCount):
+    TopKIndex = [[] for i in range(TrainResultCount)]
+    for i in range(0, TrainResultCount):
         temp = finalCSList[i]
         for j in range(0, k):
             TopKIndex[i].append(temp.index(max(temp)))
@@ -212,22 +175,18 @@ def getCosineSimilarityFromOut(filePath1, filePath2, filePath3, k):
     NDCG = 0
     AveragePrecision = 0
 
-    for i in range(0, TestitemsCount):  # 遍历每一个TestID 数据
-        topKRealDestID = []
-        RealDestID = []# 每一个TestID对应的真实地点ID列表
+    for i in range(0,TrainResultCount):
+        RealDestID = []
         RealWeight = []
-        trueNumber = 0  # 表示对第i行存在多少真实地点值的去
         RecommendDestID = []
-        for j in range(k):  # test[1，2，3，...，k-1,k]遍历每一个test
-            tempIndex = TopKIndex[i][j]
-            RecommendDestID.append(trainNodeID[TopKIndex[i][j]])  # 推荐系统认为对第i行值得去的K个地点
-
-        for line in EdgeListNode:
-            trueDestNumber = 0
-            if (testNodeID[i] == line[0]):
+        topKRealDestID = []
+        for line in TestList:
+            if (trainNodeID[i] == line[0]):
                 RealDestID.append(line[1])  # 真实对第i行值得去的K个地点
                 RealWeight.append(line[2])
-        for i in range(k):
+                for j in range(k):  # test[1，2，3，...，k-1,k]遍历每一个tuijian
+                    RecommendDestID.append(trainNodeID[TopKIndex[i][j]])  # 推荐系统认为对第i行值得去的K个地点
+        for g in range(k):
             if len(RealWeight)!=0:
                 temp = max(RealWeight)
                 index = RealWeight.index(temp)
@@ -258,15 +217,19 @@ def getCosineSimilarityFromOut(filePath1, filePath2, filePath3, k):
     F1Score = ((precisionScore * recallScore * 2) / (precisionScore + recallScore))
     NDCG = NDCG / TestitemsCount
     MAP = AveragePrecision / TestitemsCount
+    end_time = time.time()
+    cost_time = (end_time - start_time)
+    print('Total time spent on loading car data {:.5f} second.'.format(cost_time))
 
     return ans, precisionScore, recallScore, F1Score, NDCG, MAP, k
 
 
-fpath1 = "./data/CosineSimilarityData/test.txt"
-fpath2 = "./data/CosineSimilarityData/train.txt"
+testEdgelist = "E:/data/ExprimentField/test/jan/jan1/5测试图G/test.weighted.edgelist"
+trainResult = "E:/data/ExprimentField/test/jan/jan1/4训练结果/result.txt"
 fpath3 = "./data/test1.weighted.edgelist"
-"E:/data/ExprimentField/test/feb/feb1/train.csv"
-Hit, precisionScore, recallScore, F1Score, NDCG, MAP, t = getCosineSimilarityFromOut(fpath1, fpath2, fpath3, 1)
+
+
+Hit, precisionScore, recallScore, F1Score, NDCG, MAP, t = getCosineSimilarityFromOut(testEdgelist, trainResult, 10)
 # file1=open("E:/Anaconda/envs/Python35/src/code/essaycode/HYYcode/data/Percision.txt","w")
 # file1.write(str(Perc))
 # file1.close()
@@ -274,11 +237,11 @@ Hit, precisionScore, recallScore, F1Score, NDCG, MAP, t = getCosineSimilarityFro
 # file2.write(str(reca))
 # file2.close()
 print("Hit@", t, ":", Hit)
-print("PrecisionScore", t, ":", precisionScore)
-print("RecallScore", t, ":", recallScore)
-print("F1Score", t, ":", F1Score)
-print("NDCG", t, ":", NDCG)
-print("MAP", t, ":", MAP)
+print("PrecisionScore@", t, ":", precisionScore)
+print("RecallScore@", t, ":", recallScore)
+print("F1Score@", t, ":", F1Score)
+print("NDCG@", t, ":", NDCG)
+print("MAP@", t, ":", MAP)
 
 # file3 = open("E:/Anaconda/envs/Python35/src/code/essaycode/HYYcode/data/FHit.txt", "w")
 # file3.write(str(Hit))
--- essayCode/test0.py
+++ essayCode/test0.py
@@ -1,13 +1,2 @@
-import networkx as nx
-G = nx.DiGraph()
-G.add_node(1, time="5pm")
-G.add_node(2, time="6pm")
-G.add_node(3, time="7pm")
-G.add_node(4, time="8pm")
-G.add_node(5, time="9m")
-
-print(G.nodes[1]['time'])
-# for node, attr in G.nodes(data=True):
-#     frequency = attr['time']
-#     print(frequency)
-
+opKIndex = [[] for i in range(5)]
+print(opKIndex)
